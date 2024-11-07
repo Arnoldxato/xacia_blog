0121006e35a1cc781ea548086ac9df9898fe8f1e
Arnoldxato <muyoboarnold@gmail.com>
2024-11-07 16:13:08 +0300

    c

---
+++ src/Components/Content.js
@@ -0,0 +1,12 @@
+function Content(){
+
+    return(
+        <div>
+            <h1>Content</h1>
+            <p> lorem ipsum is simply dummy text of the printing and typesetting industry. lorem ipsum has been the industry's <b/>
+            standard dummy text ever since the 1500s, when an unkown printer took a galeery</p>
+        </div>
+    
+    );
+}
+export default Content;
